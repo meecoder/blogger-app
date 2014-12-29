bfd7cb89a479165ebbe6fcba4520cc3d24a3665d
Kian Moretz <meecoder@users.noreply.github.com>
2014-12-29 12:16:42 -0500

    Finish blogger section I1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,30 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body = params[:article][:body]
+        @article.save
+        flash.notice = "Article '{@article.title}' was created!"
+        redirect_to article_path(@article)
+    end
+    def destroy
+        Article.find(params[:id]).destroy
+        flash.notice = "Article '{@article.title}' was removed!"
+        redirect_to articles_path
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '{@article.title}' was updated!"
+        redirect_to article_path(@article)
+    end
+
 end
